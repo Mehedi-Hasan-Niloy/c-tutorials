7f20828fcc56a2a4c0b00b6d99096dc6cafaa3e7
Mehedi Hasan <mehedihasan1231237@gmail.com>
2026-09-05 15:33:06 +0600

    added scanf code

---
+++ scanf.c
@@ -0,0 +1,14 @@
+/*
+ write a program that an integer
+ and print that number
+*/
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Please enter your integer value : ");
+    scanf("%d",&num);
+    printf("You have pressed number %d",num);
+    return 0;  
+
+
+}
